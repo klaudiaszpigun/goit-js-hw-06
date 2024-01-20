306b4c0513c39a68fcccb859cb7ec43b01fe854a
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-01-20 11:15:56 +0100

    new

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
 // zmienna numbersOfCategories to odwołanie się do dzieci elementu ul, jest to pseudotablice
-const numberOfCategories = document.querySelectorAll("#categories .item");
+const numberOfCategories = document.querySelectorAll("#categories .item"); // całe znaczniki <li> z zawartością
 
 console.log(`Number of categories: ${numberOfCategories.length}`);
 
-allCategories.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const elementsCount = category.querySelectorAll("ul li").length;
-  console.log(`\n Category: ${categoryName}`);
-  console.log(`Elements: ${elementsCount}`);
+numberOfCategories.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContext;
+  const categoryCount = category.querySelectorAll("ul li").length;
+  console.log(`/nCategory: ${categoryName}`);
+  console.log(`Elements: ${categoryCount}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const unorderedlist = document.querySelector("#ingredients");
+
+const liArray = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  return listItem;
+});
+unorderedlist.append(...liArray);
