93f5cc7395c44b57e9bf98a6b121e2da8b68a027
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-01-20 10:34:40 +0100

    new

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,11 @@
-//ile dzieci ma znacznik <ul>
-const numberOfCategories = document.
+// zmienna numbersOfCategories to odwołanie się do dzieci elementu ul, jest to pseudotablice
+const numberOfCategories = document.querySelectorAll("#categories .item");
+
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+allCategories.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const elementsCount = category.querySelectorAll("ul li").length;
+  console.log(`\n Category: ${categoryName}`);
+  console.log(`Elements: ${elementsCount}`);
+});
