4dba060538673818fc6b5edd7b8da828956a33cd
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-01-22 20:23:44 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+// funkcja która wyrzuca losowy kolor hexagonalny
 function getRandomHexColor() {
+  // Math.floor() zaokrągla liczbę w dół, Math.random to pseudolosowa liczba od 0 do 1
   return `#${Math.floor(Math.random() * 16777215)
+    // reprezentacja hexagonalna
     .toString(16)
+    // dopełnia otrzymany ciąg z przodu zerami do uzyskania 6 znaków
     .padStart(6, 0)}`;
+}
+
+const body = document.querySelector("body");
+
+body.addEventListener("click", colorOfBody);
+
+function colorOfBody(event) {
+  body.style.backgroundColor = getRandomHexColor();
 }
