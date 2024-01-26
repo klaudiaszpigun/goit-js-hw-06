f9b22e05fd23f53fe5d003585b884dd3cc362fa8
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-01-26 13:42:52 +0100

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,18 @@
 const validationInput = document.querySelector("#validation-input");
-
+const value = document.querySelector("input[data-length]");
 // dodanie zdarzenia blur, czyli po zniknięciu focusu
 validationInput.addEventListener("blur", () => {
   // zmienna valueLength to długość watrości wprowadzonej do inputu
   const valueLength = validationInput.value.length;
   // element.classList.toggle(className, condition);
   // jeśli warunek został spełniony to dodaje do elementu klase valid
-  validationInput.classList.toggle("valid", valueLength >= 6);
+  validationInput.classList.toggle(
+    "valid",
+    valueLength >= value.dataset.length
+  );
   // jeśli warunek został spełniony to dodaje do elementu klasę invalid
-  validationInput.classList.toggle("invalid", valueLength < 6);
+  validationInput.classList.toggle(
+    "invalid",
+    valueLength < value.dataset.length
+  );
 });
